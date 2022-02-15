33e7f3ae63a12ea6d4e039f39a549c99dc539dd2
Gisse88 <coronel.dg@gmail.com>
2022-02-15 17:32:05 -0300

    inputs editables

--- admUsuarios/public/js/users.js
+++ admUsuarios/public/js/users.js
@@ -10,6 +10,7 @@ fetch("/api/users")
     })
     .then(function(data){
          var users = document.getElementById("users")
+    
         //console.log(data)
         if (data.length ===0){
             users.innerHTML = users.innerHTML + `<div class="users_title">
@@ -28,36 +29,65 @@ fetch("/api/users")
                     </div>
                      <div class="user__info">
                         <input type="hidden" name="idSelected" value=${user.idusuario}>
-                        <p class="user__name">${user.name}</p>
-                        <p class="user__gender">${user.gender}</p>
-                        <p class="user__age">${user.age}</p>
-                        <p class="user__email">${user.email}</p>
-                        <p class="user__profession">${user.profession}</p>
-                        <p class="user__salary">${user.salary}</p>
+                        <input class="user__input" disabled value=${user.name}>
+                        <input class="user__input" disabled value=${user.gender}>
+                        <input class="user__input" disabled value=${user.age}>
+                        <input class="user__input" disabled  value=${user.email}>
+                        <input class="user__input" disabled value=${user.profession}>
+                        <input class="user__input" disabled value=${user.salary}>
+                        <button class="btn-edit" id="btn-edit" onclick="changeEdit()">Editar</button>
+
                     </div>
                 </div>
                         
                 <div class="user__btn">
-                    <button class="btn">Editar</button>
+                    <button class="btn btn-update" style={visibility=hidden}" onclick="updateUser(${user.idusuario})">Guardar</button>
                     <button class="btn" onclick="deleteUser(${user.idusuario})">Eliminar</button>
                 </div>
                         
             </div>`
             }
+    })
+
+    // delete user
+    async function deleteUser(id){
+        console.log("Quiero eliminar usuario "+id);
+        fetch("/api/users/"+id,{
+            method: "DELETE",
         })
+        .then(function(respuesta){
+            //console.log(respuesta)
+            location.reload();
+            return respuesta.json()
+        })       
+    }
 
-       // delete user
-       async function deleteUser(id){
-            console.log("Quiero eliminar usuario "+id);
-            fetch("/api/users/"+id,{
-                method: "DELETE",
-            })
-            .then(function(respuesta){
-                //console.log(respuesta)
-                location.reload();
-                return respuesta.json()
-            })
-            
+    // update user
+    function changeEdit(){
+        let inputs = document.getElementsByClassName("user__input");
+        for (var input of inputs){
+            input.disabled = false;
+        }
+        /*
+        document.getElementsByClassName("user__name")[0].disabled = false;
+        document.getElementsByClassName("user__gender")[0].disabled = false;
+        document.getElementsByClassName("user__age")[0].disabled = false;
+        document.getElementsByClassName("user__email")[0].disabled = false;
+        document.getElementsByClassName("user__profession")[0].disabled = false;
+        document.getElementsByClassName("user__salary")[0].disabled = false;*/
 
+        document.getElementById('btn-edit').style.visibility='hidden';
+    }
+   
+    async function updateUser(id){
+        console.log("Quiero actualizar usuario "+id);
+        fetch("/api/users/"+id,{
+            method: "POST",
+        })
+        .then(function(respuesta){
+            //console.log(respuesta)
+            location.reload();
+            return respuesta.json()
+        })       
     }
        
